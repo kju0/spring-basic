2f8d2f8a90564ea1606efb351f5716e45f508695
jujulia3040@gmail.com <jujulia3040@gmail.com>
2024-06-07 15:45:40 +0900

    Fix: 예외처리 추가

--- src/main/java/jungle/board/controller/PostController.java
+++ src/main/java/jungle/board/controller/PostController.java
@@ -2,9 +2,12 @@ package jungle.board.controller;
 
 import jungle.board.dto.PostRequestDto;
 import jungle.board.dto.PostResponseDto;
+import jungle.board.dto.ResponseDto;
 import jungle.board.entity.Post;
 import jungle.board.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -21,27 +24,45 @@ public class PostController {
     }
 
     @PostMapping("/posts")
-    public PostResponseDto createPost(@RequestBody PostRequestDto requestDto) {
-        return postService.createPost(requestDto);
+    public ResponseEntity<ResponseDto<PostResponseDto>> createPost(@RequestBody PostRequestDto requestDto) {
+        try{
+            PostResponseDto postResponseDto = postService.createPost(requestDto);
+            ResponseDto<PostResponseDto> responseDto = ResponseDto.ofSuccess("Post created successfully", postResponseDto);
+            return new ResponseEntity<>(responseDto, HttpStatus.OK);
+        } catch (Exception e) {
+            ResponseDto<PostResponseDto> responseDto = ResponseDto.ofFail("Failed to create post", null);
+            return new ResponseEntity<>(responseDto, HttpStatus.BAD_REQUEST);
+        }
     }
 
     @GetMapping("/posts")
-    public List<Post> getPosts() {
+    public List<PostResponseDto> getPosts() { //todo: responseEntity로 감싸줘야할까?
         return postService.getPosts();
     }
 
     @GetMapping("/posts/{id}")
-    public Post getPostDetail(@PathVariable Long id) {
+    public PostResponseDto getPostDetail(@PathVariable Long id) {
         return postService.getPostDetail(id);
     }
 
     @PutMapping("/posts/{id}")
-    public Long updatePost(@PathVariable Long id, @RequestBody PostRequestDto requestDto) {
-        return postService.update(id, requestDto);
+    public ResponseEntity<ResponseDto<Long>> updatePost(@PathVariable Long id, @RequestBody PostRequestDto requestDto) {
+        if (postService.update(id, requestDto)){
+            ResponseDto<Long> responseDto = ResponseDto.ofSuccess("Post updated successfully", id);
+            return new ResponseEntity<>(responseDto, HttpStatus.OK);
+        }
+
+        ResponseDto<Long> responseDto = ResponseDto.ofFail("Failed to update post", id);
+        return new ResponseEntity<>(responseDto, HttpStatus.BAD_REQUEST);
     }
 
     @DeleteMapping("/posts/{id}")
-    public Long deletePost(@PathVariable Long id) {
-        return postService.deletePost(id);
+    public ResponseEntity<ResponseDto<Long>> deletePost(@PathVariable Long id, @RequestBody PostRequestDto requestDto) {
+        if (postService.deletePost(id, requestDto)){
+            ResponseDto<Long> responseDto = ResponseDto.ofSuccess("Post deleted successfully", id);
+            return new ResponseEntity<>(responseDto, HttpStatus.OK);
+        }
+        ResponseDto<Long> responseDto = ResponseDto.ofSuccess("Failed to delete post", id);
+        return new ResponseEntity<>(responseDto, HttpStatus.BAD_REQUEST);
     }
 }
