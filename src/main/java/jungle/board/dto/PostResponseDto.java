51ea810902ad0ee44243416723bbcaab4a61aaae
jujulia3040@gmail.com <jujulia3040@gmail.com>
2024-06-07 15:43:12 +0900

    Feat: PostResponseDto id 추가

--- src/main/java/jungle/board/dto/PostResponseDto.java
+++ src/main/java/jungle/board/dto/PostResponseDto.java
@@ -5,11 +5,13 @@ import lombok.Getter;
 
 @Getter
 public class PostResponseDto {
+    private Long id;
     private String title;
     private String author;
     private String contents;
 
     public PostResponseDto(Post post) {
+        this.id = post.getId();
         this.title = post.getTitle();
         this.author = post.getAuthor();
         this.contents = post.getContents();
